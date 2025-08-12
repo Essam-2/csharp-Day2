73793b3c80793899b4d6aa67ab65c3e9f2859f3a
Essam <esamanwar2@gmail.com>
2025-08-12 22:27:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+
+class Account
+{
+    private string name = "Essam";
+
+    public void SetName (string accountName)
+    {
+        name = accountName;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+}
+
+
+class AccountTest
+{
+    static void Main()
+    {
+        // create an Account object and assing iy to myAccount 
+        Account myAccount = new Account();
+
+        Console.WriteLine($"Initial name is: {myAccount.GetName()}");
+        Console.WriteLine("Enter the name");
+
+
+        string theName = Console.ReadLine();
+
+        myAccount.SetName(theName);
+
+        Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+
+
+
+
+
+
+        Console.ReadLine();
+    }
+}
